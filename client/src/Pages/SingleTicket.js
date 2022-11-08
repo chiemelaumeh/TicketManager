1edb45ac9d3048262a347dfa7449b9666b17b64e
Blackflame6 <engineerfranklyn@gmail.com>
2022-11-07 19:11:19 -0600

    done

---
+++ client/src/Pages/SingleTicket.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+const SingleTicket = () => {
+  const {ticketId} = useParams()
+  return (
+    <section className='singleTicketPage'>
+
+      <h1>SingleTicket</h1>
+      <h4>{ticketId}</h4>
+      <Link to="/tech"> Tickets</Link>
+    </section>
+
+  )
+}
+
+export default SingleTicket
+
