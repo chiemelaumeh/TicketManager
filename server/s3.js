e54f53836ecec9feceffc71714ce08471d37928c
WyattReed01 <wickdshot@gmail.com>
2022-11-07 18:13:52 -0600

    s3url for pics, first init commit

---
+++ server/s3.js
@@ -0,0 +1,32 @@
+const aws = require("aws-sdk") ;
+const crypto = require('crypto');
+const {promisify} = require("util")
+const randomBytes = promisify(crypto.randomBytes)
+
+require('dotenv').config()
+
+const region = "US East (N. Virginia) us-east-1";
+const bucketName = "blue-digitial-bucket";
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new aws.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: "v4",
+});
+
+exports.generateUploadURL = async ()=> {
+    const rawBytes = await randomBytes(16)
+    const imageName = rawBytes.toString('hex')
+
+  const params = {
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60,
+  };
+
+  const uploadURL = await s3.getSignedUrlPromise("putObject", params);
+  return uploadURL;
+}
