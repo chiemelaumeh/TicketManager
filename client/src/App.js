19c77c9133b0545dabdec9b3d26b57a890f37506
unknown <melvinlanza16@gmail.com>
2022-11-10 10:11:53 -0800

    added create ticket and ticket table on user page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import LoginContext from "./Contexts/loginContext";
 import { useContext } from "react";
 import "./CssFiles/login.css"
 import "./CssFiles/TicketHistory.css"
+import './CssFiles/user.css'
 
 import CreateAccount from "./Components/Admin/CreateAccount";
 import ManageAccounts from "./Components/Admin/ManageAccounts";
