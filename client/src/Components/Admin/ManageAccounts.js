2c5ccd7e95e4427e4002f50100dae67b28ca85e0
abelardom3 <abelardom1999@gmail.com>
2022-11-14 18:59:01 -0500

    added ticket history

--- client/src/Components/Admin/ManageAccounts.js
+++ client/src/Components/Admin/ManageAccounts.js
@@ -1,26 +1,26 @@
 import fakeData from "../../Pages/dataMA"
 import DataTable from "../../Components 2/DataTable"
-import {useContext} from "react"
+import { useContext } from "react"
 import "../../CssFiles/manageacc.css"
 
 
 const ManageAccounts = () => {
-//    const {fakeData} = useContext()
-return (
+    //    const {fakeData} = useContext()
+    return (
 
-    <div>
-        <h2>Manage Accounts</h2>
-<div className="Header">
-    <div>FullName</div>
-    <div>Email</div>
-    <div>Campus</div>
-</div>
-        {fakeData.map((data) => (
-            <DataTable key={data.full_name} data={data}/>
-           
-        ))}
-    </div>
-)
+        <div>
+            <h2>Manage Accounts</h2>
+            <div className="Header">
+                <div>FullName</div>
+                <div>Email</div>
+                <div>Campus</div>
+            </div>
+            {fakeData.map((data) => (
+                <DataTable key={data.full_name} data={data} />
+
+            ))}
+        </div>
+    )
 }
 
 export default ManageAccounts
--- client/src/Components/Admin/TicketHistory.js
+++ client/src/Components/Admin/TicketHistory.js
@@ -1,18 +1,37 @@
+import AdminSingle from "./AdminSingle"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 
 const TicketHistory = () => {
+
+    const [ticket, setTicket] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get('http://localhost:6001/admin/tickets')
+            setTicket(result.data)
+        }
+        fetchData()
+    }, [])
+
     return (
-        <div class="table-container" role="table">
-            <div class="table-col table-text" >Ticket Number</div>
-            <div class="table-col table-text" >Submission</div>
-            <div class="table-col table-text" >Category</div>
-            <div class="table-col table-text" >Urgency</div>
-            <div class="table-col table-text" >Your Tech</div>
-            <div class="table-col table-text" >Status</div>
-            <div class="table-col table-text" >ETR</div>
+        <>
+            <div className="table-container">
+                <div className="table-text" >Ticket#</div>
+                <div className="table-text" >Campus</div>
+                <div className="table-text" >Category</div>
+                <div className="table-text" >Urgency</div>
+                <div className="table-text" >Status</div>
+                <div className="table-text" >ETR</div>
+            </div>
+            {ticket.map((elem) => (
+                <AdminSingle elem={elem} key={elem.ticket_id} />
+            ))}
+        </>
 
-
-        </div>
     )
 }
 
