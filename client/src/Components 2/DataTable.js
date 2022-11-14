c8ef404a2fee6f00ec2ad7e9f668e99e60c88898
Meka James <mekajames@Mekas-MBP.attlocal.net>
2022-11-14 16:32:56 -0600

    Add to managing account

--- client/src/Components 2/DataTable.js
+++ client/src/Components 2/DataTable.js
@@ -1,20 +1,31 @@
-import fakeData from "../Pages/dataMA"
-const DataTable = ({data}) => {
+
+import { BsTrash } from "react-icons/bs"
+import { FaPencilAlt } from "react-icons/fa"
+
+const DataTable = ({ data }) => {
     // console.log(data)
-    return(
+
+    
+    return (
         <>
-        <div>
-            <div className="Data_Table">
-            <div >{data.full_name}</div>
-            <div >{data.email}</div>
-            <div >{data.camp_id}</div>
-           
+            <div>
+                <div id={data.user_id} className="Data_Table">
+                    
+                    <div >{data.username}</div>
+                    <div >{data.email}</div>
+                    <div >{data.accessrole}</div>
+                    <div >{data.campus_name}</div>
+                    <div>
+                        <div ><FaPencilAlt /></div>
+                        <div ><BsTrash /></div>
+                    </div>
+                </div>
+
             </div>
-            
-        </div>
 
         </>
     )
-    }
-    
-    export default DataTable
+}
+
+export default DataTable
+// /admin/Accounts
--- client/src/Components/Admin/ManageAccounts.js
+++ client/src/Components/Admin/ManageAccounts.js
@@ -1,26 +1,42 @@
-import fakeData from "../../Pages/dataMA"
+
 import DataTable from "../../Components 2/DataTable"
-import {useContext} from "react"
 import "../../CssFiles/manageacc.css"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 const ManageAccounts = () => {
 //    const {fakeData} = useContext()
+const [account, setAccount] = useState([])
+   useEffect(() => {
+const getDatatFromDB = async() => {
+const {data} = await axios.get("http://localhost:6001/admin/Accounts");
+setAccount(data)
+console.log(data)
+}
+getDatatFromDB()
+   }, [])
+
 return (
 
     <div>
         <h2>Manage Accounts</h2>
 <div className="Header">
-    <div>FullName</div>
+    <div>UserName</div>
     <div>Email</div>
+    <div>Access</div>
     <div>Campus</div>
 </div>
-        {fakeData.map((data) => (
-            <DataTable key={data.full_name} data={data}/>
+        {account.map((data) => (
+            <DataTable key={data.user_id} data={data}/>
            
-        ))}
+        ))} 
     </div>
 )
 }
 
 export default ManageAccounts
+// user_id SERIAL PRIMARY KEY NOT NULL,
+//     userName TEXT,
+//     accessRole TEXT,
+//     campus_name TEXT,
