096a12758e649c8fd7637fcc385a74ebc48e4187
abelardom3 <abelardom1999@gmail.com>
2022-11-14 20:51:04 -0500

    added state to create account

--- client/src/Components/Admin/CreateAccount.js
+++ client/src/Components/Admin/CreateAccount.js
@@ -1,25 +1,48 @@
+import { useState } from "react"
+import axios from 'axios'
 import "../../CssFiles/admin.css"
+
 const CreateAccount = () => {
-return (
-    <div className="AdminCreateAccount">
-        <h2 id = "CreateAccount">CREATE ACCOUNT</h2>
-        <input placeholder="Full Name" className="inputBox"></input>
-        <input placeholder="EMAIL" className="inputBox"></input>
-        <select className="inputBox">
-            <option>San Antonio</option>
-            <option>Austin</option>
-            <option>Houston</option>
-            <option>Dallas</option>
-        </select>
-        <select className="inputBox">
-            <option>ADMIN</option>
-            <option>TECH</option> 
-            <option>USER</option>
-        </select>
-        <input placeholder="Initial Password" className="inputBox"></input>
-        <button id="AdminCreateAccountBTN">CREATE ACCOUNT</button>
-    </div>
-)
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        campus: 'San Antonio',
+        role: 'ADMIN',
+        password: ''
+    })
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const result = await axios.post('http://localhost:6001/account/register', values)
+        console.log(result.data)
+    }
+
+    const handleChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+
+
+    return (
+        <form className="AdminCreateAccount" onSubmit={onSubmit}>
+            <h2 id="CreateAccount">CREATE ACCOUNT</h2>
+            <input type='text' placeholder="Full Name" className="inputBox" onChange={handleChange} value={values.name} name='name' />
+            <input type='text' placeholder="EMAIL" className="inputBox" onChange={handleChange} value={values.email} name='email' />
+            <select className="inputBox" onChange={handleChange} value={values.campus} name='campus' >
+                <option value='San Antonio'>San Antonio</option>
+                <option value='Austin'>Austin</option>
+                <option value='Houston'>Houston</option>
+                <option value='Dallas'>Dallas</option>
+            </select>
+            <select className="inputBox" onChange={handleChange} value={values.role} name='role' >
+                <option value='ADMIN'>ADMIN</option>
+                <option value='TECH'>TECH</option>
+                <option value='USER'>USER</option>
+            </select>
+            <input type='password' placeholder="Initial Password" className="inputBox" onChange={handleChange} value={values.password} name='password' />
+            <button id="AdminCreateAccountBTN" type="submit">CREATE ACCOUNT</button>
+        </form>
+    )
 }
 
 //give the option a value= "option 1"
--- server/AuthRoute/register_login.js
+++ server/AuthRoute/register_login.js
@@ -6,44 +6,46 @@ require('dotenv').config()
 
 const authRoute = express.Router();
 
-authRoute.post('/register', async (req,res) => {
-    const {userName, accessRole, campus_name, email, password} = req.body;
+authRoute.post('/register', async (req, res) => {
+    // const { userName, accessRole, campus_name, email, password } = req.body;
+    const { name, role, campus, email, password } = req.body;
+    res.send('hit register backend')
 
-    if(userName === "" || accessRole === "" || campus_name === "" || email === "" || password === "") return res.send('Fields cannot be empty');
+    // if(userName === "" || accessRole === "" || campus_name === "" || email === "" || password === "") return res.send('Fields cannot be empty');
 
-    //check if email exists; make database query and filter to check if email exists
-    const {rows} = await pool.query('select * from accounts');
-    const account = rows.filter(account => account.email === email);
+    // //check if email exists; make database query and filter to check if email exists
+    // const {rows} = await pool.query('select * from accounts');
+    // const account = rows.filter(account => account.email === email);
 
-    //if account is NOT empty accounts already exist
-    if(account.length !== 0){
-        return res.send('account already exists');
-    }else{
-        try {
-            //create salt rounds
-            const salt = await bcrypt.genSalt(10);
-            //hash the password with salt rounds
-            const hashedPassword = await bcrypt.hash(password,salt);
-            //insert input data into database with hashed password, NOT typed password
-            const {rows} = await pool.query('insert into accounts (userName, accessRole, campus_name, email, password) values ($1,$2,$3,$4,$5) returning *', [userName, accessRole, campus_name, email, hashedPassword]);
-            res.send(rows);
-        } catch (error) {
-            res.status(404).send(error.message);
-        }
-    }
+    // //if account is NOT empty accounts already exist
+    // if(account.length !== 0){
+    //     return res.send('account already exists');
+    // }else{
+    //     try {
+    //         //create salt rounds
+    //         const salt = await bcrypt.genSalt(10);
+    //         //hash the password with salt rounds
+    //         const hashedPassword = await bcrypt.hash(password,salt);
+    //         //insert input data into database with hashed password, NOT typed password
+    //         const {rows} = await pool.query('insert into accounts (userName, accessRole, campus_name, email, password) values ($1,$2,$3,$4,$5) returning *', [userName, accessRole, campus_name, email, hashedPassword]);
+    //         res.send(rows);
+    //     } catch (error) {
+    //         res.status(404).send(error.message);
+    //     }
+    // }
 });
 
 authRoute.post('/login', async (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     //check for empty request values
-    if(email === "" || password === "") return res.send('Fields cannot be empty');
+    if (email === "" || password === "") return res.send('Fields cannot be empty');
 
     //check if email exists
-    const {rows} = await pool.query('select * from accounts');
+    const { rows } = await pool.query('select * from accounts');
     const account = rows.filter(account => account.email === email);
 
     //if account length is 0 then there is no account found
-    if(account.length === 0) return res.status(404).send("account not found");
+    if (account.length === 0) return res.status(404).send("account not found");
 
     try {
         //create variable for found account and access at index
@@ -52,14 +54,14 @@ authRoute.post('/login', async (req, res) => {
         const isAuthorized = await bcrypt.compare(password, user.password)
 
         //if password matches create secret token and respond with token; else return incorrect password
-        if(isAuthorized === true){
+        if (isAuthorized === true) {
             const secretToken = jwt.sign({
                 user_id: user.user_id,
                 userName: user.username,
                 email: user.email,
                 password: user.password
             }, process.env.ACCESS_TOKEN);
-            
+
             res.status(200).send({
                 user_id: user.user_id,
                 userName: user.username,
@@ -68,8 +70,8 @@ authRoute.post('/login', async (req, res) => {
                 campus_name: user.campus_name,
                 accessToken: secretToken
             });
-            
-        }else if(isAuthorized === false){
+
+        } else if (isAuthorized === false) {
             res.status(401).send("Incorrect Password")
         }
     } catch (error) {
