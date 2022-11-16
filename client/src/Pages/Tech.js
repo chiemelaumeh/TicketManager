c282db945828ea96a019634f2ba136526c537b74
abelardom3 <abelardom1999@gmail.com>
2022-11-15 19:59:55 -0500

    added filter to tech

--- client/src/Pages/Tech.js
+++ client/src/Pages/Tech.js
@@ -4,18 +4,25 @@ import Navbar from "../Components/Navbar";
 import { useContext, useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
-import { FaRegBuilding} from "react-icons/fa";
+import { FaRegBuilding } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 import { FaGear } from "react-icons/fa";
 import { AiFillSetting } from "react-icons/ai";
-import { FaTicketAlt} from "react-icons/fa";
+import { FaTicketAlt } from "react-icons/fa";
 import TechContext from "../Contexts/TechPageContext";
 import axios from "axios";
 
 const Tech = () => {
 
 
-  const {tickets, setTickets} = useContext(TechContext)
+  const { tickets, setTickets } = useContext(TechContext)
+
+  const [searchText, setSearchText] = useState('')
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value)
+    console.log(searchText)
+  }
 
 
   const [open, setOpen] = useState(true);
@@ -34,30 +41,30 @@ const Tech = () => {
 
   useEffect(() => {
     const getTickets = async () => {
-      const {data} = await axios.get(`http://localhost:6001/tech/Tickets/campus/1`)
+      const { data } = await axios.get(`http://localhost:6001/tech/Tickets/campus/1`)
 
       setTickets(data)
     }
     getTickets()
   }, [])
 
-  
+
 
 
 
 
   return (
     <>
-      <Navbar />
+      <Navbar handleSearch={handleSearch} searchText={searchText} />
       <div className="mainticket">
         <div className={open ? "open-sidenav" : "close-sidenav"}>
-         
+
           <FaBars className="menubars" onClick={handleClick} />
           <div className={open ? "menu-items" : "menu-items-hide"}>
-            <li className="menu-list">HOME <FaHome className="menu-icon"/></li>
-            <li className="menu-list">TICKETS <FaTicketAlt className="menu-icon"/></li>
-            <li className="menu-list">RESOLVED <FaCheck className="menu-icon"/></li>
-            <li className="menu-list">SETTINGS <AiFillSetting className="menu-icon"/></li>
+            <li className="menu-list">HOME <FaHome className="menu-icon" /></li>
+            <li className="menu-list">TICKETS <FaTicketAlt className="menu-icon" /></li>
+            <li className="menu-list">RESOLVED <FaCheck className="menu-icon" /></li>
+            <li className="menu-list">SETTINGS <AiFillSetting className="menu-icon" /></li>
 
           </div>
         </div>
@@ -65,7 +72,15 @@ const Tech = () => {
           <h3 className="portal-text">TECH PORTAL</h3>
           <div className="all-tickets">
             <div className="claimed-tickets">
-              {claimed.map((tickets) => {
+              {claimed.filter((value) => {
+                if (searchText === '') {
+                  return value;
+                } else if (value.category.toLowerCase().includes(searchText.toLowerCase()) ||
+                  value.ticket_id.toString().toLowerCase().includes(searchText.toLowerCase())
+                ) {
+                  return value;
+                }
+              }).map((tickets) => {
                 return (
                   <article
                     className="ticket unclaimed-oneticket"
@@ -79,7 +94,15 @@ const Tech = () => {
               })}
             </div>
             <div className="unclaimed-tickets" >
-              {unclaimed.map((tickets) => {
+              {unclaimed.filter((value) => {
+                if (searchText === '') {
+                  return value;
+                } else if (value.category.toLowerCase().includes(searchText.toLowerCase()) ||
+                  value.ticket_id.toString().toLowerCase().includes(searchText.toLowerCase())
+                ) {
+                  return value;
+                }
+              }).map((tickets) => {
                 return (
                   <article
                     className="ticket claimed-oneticket"
