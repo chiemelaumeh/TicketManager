99648d030f8efe41ee6c8b7143697882432d7878
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2022-11-17 17:57:19 -0500

    added popup

--- client/src/Portals/ManageInput.js
+++ client/src/Portals/ManageInput.js
@@ -1,21 +1,18 @@
 import DataTable from "../Components 2/DataTable"
 import ReactDOM from "react-dom"
-const Manage = ({ close, open }) => {
-
-
+const Manage = ({ close, open, data }) => {
     if (!open) return null
     return ReactDOM.createPortal(
         <><div className="over-lay">
             <div className="module-box">
-                <div className="header-profile">
-                    
+                <div >
                     <button onClick={() => close()}>X</button>
                 </div>
-                <DataTable />
+                {/* <DataTable /> */}
                 <button>save changes</button>
+                <h1>{data.username}</h1>
             </div>
         </div>
-        </>, document.getElementById('Data_Table'))
+        </>, document.getElementById('portal'))
 }
-
 export default Manage
