41bc577cc1c29525d45a0a72f38ff1b7f36d84b4
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-11-17 16:37:43 -0700

    Manage Account and Ticket History css

--- client/src/Components/Admin/TicketHistory.js
+++ client/src/Components/Admin/TicketHistory.js
@@ -18,19 +18,22 @@ const TicketHistory = () => {
     }, [])
 
     return (
-        <>
+        <div className="ManageDiv">
+        <div>
+            <h2 className="ManageAccountH2">Ticket History</h2>
+        </div>
             <div className="table-container">
-                <div className="table-text" >Ticket#</div>
-                <div className="table-text" >Campus</div>
-                <div className="table-text" >Category</div>
-                <div className="table-text" >Urgency</div>
-                <div className="table-text" >Status</div>
-                <div className="table-text" >ETR</div>
+                <h2 className="table-text" >Ticket#</h2>
+                <h2 className="table-text" >Campus</h2>
+                <h2 className="table-text" >Category</h2>
+                <h2 className="table-text" >Urgency</h2>
+                <h2 className="table-text" >Status</h2>
+                <h2 className="table-text" >ETR</h2>
             </div>
             {ticket.map((elem) => (
                 <AdminSingle elem={elem} key={elem.ticket_id} />
             ))}
-        </>
+        </div>
 
     )
 }
--- client/src/Components/Admin/header.js
+++ client/src/Components/Admin/header.js
@@ -10,12 +10,12 @@ const Header = () => {
     return (
         <> 
         <h1 className="NavBarTitle">ATHENA</h1>
+            <button onClick={handleLogOut} id="LogOutBTN">log out</button>
         <nav className="NavBar">
             <NavLink to="/admin/CreateAccount" className="NavBarComp">Create Accout</NavLink>
             <NavLink to="/admin/ManageAccounts" className="NavBarComp">ManageAccounts</NavLink>
             <NavLink to="/admin/TicketHistory" className="NavBarComp">TicketHistory</NavLink>
         </nav>
-            <button onClick={handleLogOut} id="LogOutBTN">log out</button>
         </>
     )
 }
