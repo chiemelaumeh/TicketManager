24644c7738c0529b743be5d93b91a9df34efb1b6
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2022-11-21 19:41:04 -0500

    admin css

--- client/src/Portals/ManageInput.js
+++ client/src/Portals/ManageInput.js
@@ -1,43 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-
-
 const Manage = ({ close, open, data, reRender }) => {
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
 
   const editOne = async (id) => {
-
-    const { data } = await axios.patch(`http://localhost:6001/admin/Accounts/edit/${id}`, { userName, email });
-    setUserName('')
-    setEmail('')
-    reRender()
-  }
+    const { data } = await axios.patch(
+      `http://localhost:6001/admin/Accounts/edit/${id}`,
+      { userName, email }
+    );
+    setUserName("");
+    setEmail("");
+    reRender();
+  };
   const handleUsername = (e) => {
-    setUserName(e.target.value)
-   
-  }
+    setUserName(e.target.value);
+  };
   const handleEmail = (e) => {
-    setEmail(e.target.value) 
+    setEmail(e.target.value);
     // console.log(e.target.value)
-  }
+  };
   if (!open) return null;
   return ReactDOM.createPortal(
     <>
       <div className="over-lay">
         <div className="module-box">
-          <div>
-            <button onClick={() => close()}>X</button>
-          </div>
-          
-          <button onClick={() => editOne(data.user_id)}>save changes</button>
-          <form>
+          <form className="editForm">
             <p>Current Name: {data.username}</p>
-            <label>Edit Name: </label><input type='text' onChange={handleUsername} value={userName}></input>
+            <label className="editLabels">Edit Name: </label>
+            <input
+              type="text"
+              onChange={handleUsername}
+              value={userName}
+            ></input>
             <p>Current Email: {data.email}</p>
-            <label>Edit Email: </label><input type='text' onChange={handleEmail} value={email}></input>
+            <label>Edit Email: </label>
+            <input type="text" onChange={handleEmail} value={email}></input>
+            <br></br>
+            <div>
+              <button className="editBtns" onClick={() => close()}>
+                Close
+              </button>
+              <button
+                className="editBtns"
+                onClick={() => editOne(data.user_id)}
+              >
+                Save Changes
+              </button>
+            </div>
           </form>
         </div>
       </div>
